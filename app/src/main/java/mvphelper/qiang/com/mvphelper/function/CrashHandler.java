bd9e63dbd88b929dba7fb1b86e5d5dea73063d5d
liqiang <liqiang@6.cn>
2017-12-13 13:06:47 +0800

    日志崩溃数据库收集

--- app/src/main/java/mvphelper/qiang/com/mvphelper/function/CrashHandler.java
+++ app/src/main/java/mvphelper/qiang/com/mvphelper/function/CrashHandler.java
@@ -38,90 +38,90 @@ import static mvphelper.qiang.com.mvphelper.base.Constant.STORAGE_PATH;
  */
 public class CrashHandler implements UncaughtExceptionHandler {
 
-	public static final String TAG = "CrashHandler";
+    public static final String TAG = "CrashHandler";
 
-	//系统默认的UncaughtException处理类
-	private UncaughtExceptionHandler mDefaultHandler;
-	//CrashHandler实例
-	private static CrashHandler INSTANCE = new CrashHandler();
-	//程序的Context对象
-	private Context mContext;
-	//用来存储设备信息和异常信息
-	private Map<String, String> infos = new ArrayMap<String, String>();
+    //系统默认的UncaughtException处理类
+    private UncaughtExceptionHandler mDefaultHandler;
+    //CrashHandler实例
+    private static CrashHandler INSTANCE = new CrashHandler();
+    //程序的Context对象
+    private Context mContext;
+    //用来存储设备信息和异常信息
+    private Map<String, String> infos = new ArrayMap<String, String>();
 
-	//用于格式化日期,作为日志文件名的一部分
-	private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	private ContentValues contentValues;
+    //用于格式化日期,作为日志文件名的一部分
+    private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private ContentValues contentValues;
 
-	private CrashHandler() {
-	}
+    private CrashHandler() {
+    }
 
-	/**
-	 * 获取CrashHandler实例 ,单例模式
-	 */
-	public static CrashHandler getInstance() {
-		return INSTANCE;
-	}
+    /**
+     * 获取CrashHandler实例 ,单例模式
+     */
+    public static CrashHandler getInstance() {
+        return INSTANCE;
+    }
 
-	/**
-	 * 初始化
-	 *
-	 * @param context
-	 */
-	public void init(Context context) {
-		mContext = context;
-		//获取系统默认的UncaughtException处理器
-		mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
-		//设置该CrashHandler为程序的默认处理器
-		Thread.setDefaultUncaughtExceptionHandler(this);
-	}
+    /**
+     * 初始化
+     *
+     * @param context
+     */
+    public void init(Context context) {
+        mContext = context;
+        //获取系统默认的UncaughtException处理器
+        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        //设置该CrashHandler为程序的默认处理器
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
 
-	/**
-	 * 当UncaughtException发生时会转入该函数来处理
-	 */
-	@Override
-	public void uncaughtException(Thread thread, Throwable ex) {
-		if (!handleException(ex) && mDefaultHandler != null) {
-			//如果用户没有处理则让系统默认的异常处理器来处理
-			mDefaultHandler.uncaughtException(thread, ex);
-		} else {
-			android.os.Process.killProcess(android.os.Process.myPid());
-			System.exit(1);
+    /**
+     * 当UncaughtException发生时会转入该函数来处理
+     */
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        if (!handleException(ex) && mDefaultHandler != null) {
+            //如果用户没有处理则让系统默认的异常处理器来处理
+            mDefaultHandler.uncaughtException(thread, ex);
+        } else {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-	 *
-	 * @param ex
-	 * @return true:如果处理了该异常信息;否则返回false.
-	 */
-	private boolean handleException(Throwable ex) {
-		if (ex == null) {
-			return false;
-		}
-		//收集设备参数信息
-		collectDeviceInfo(mContext);
-		//保存日志文件
-		saveCrashInfo2File(ex);
-		return true;
-	}
+    /**
+     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
+     *
+     * @param ex
+     * @return true:如果处理了该异常信息;否则返回false.
+     */
+    private boolean handleException(Throwable ex) {
+        if (ex == null) {
+            return false;
+        }
+        //收集设备参数信息
+        collectDeviceInfo(mContext);
+        //保存日志文件
+        saveCrashInfo2File(ex);
+        return true;
+    }
 
-	/**
-	 * 收集设备参数信息
-	 *
-	 * @param ctx
-	 */
-	public void collectDeviceInfo(Context ctx) {
-		//版本Version；渠道Channel；型号Model；系统System；时间Time；崩溃日志Log
-		 contentValues = new ContentValues();
-		 contentValues.put("Version",SystemUtil.getAppVersion()+"");
-		 contentValues.put("Channel","");
-		 contentValues.put("Model",Build.MODEL);
-		 contentValues.put("System",SystemUtil.getSystemVersion());
-		 contentValues.put("Time",formatter.format(new Date()));
-		/*try {
+    /**
+     * 收集设备参数信息
+     *
+     * @param ctx
+     */
+    public void collectDeviceInfo(Context ctx) {
+        //版本Version；渠道Channel；型号Model；系统System；时间Time；崩溃日志Log
+        contentValues = new ContentValues();
+        contentValues.put("Version", SystemUtil.getAppVersion() + "");
+        contentValues.put("Channel", "");
+        contentValues.put("Model", Build.MODEL);
+        contentValues.put("System", SystemUtil.getSystemVersion());
+        contentValues.put("Time", formatter.format(new Date()));
+        /*try {
 			PackageManager pm = ctx.getPackageManager();
 			PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(), PackageManager.GET_ACTIVITIES);
 			if (pi != null) {
@@ -143,30 +143,30 @@ public class CrashHandler implements UncaughtExceptionHandler {
 				Log.e(TAG, "收集信息时发生错误", e);
 			}
 		}*/
-	}
+    }
 
 
-	/**
-	 * 保存错误信息到文件中
-	 *
-	 * @param ex
-	 * @return 返回文件名称, 便于将文件传送到服务器
-	 */
-	private void saveCrashInfo2File(Throwable ex) {
+    /**
+     * 保存错误信息到文件中
+     *
+     * @param ex
+     * @return 返回文件名称, 便于将文件传送到服务器
+     */
+    private void saveCrashInfo2File(Throwable ex) {
 
-		Writer writer = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(writer);
-		ex.printStackTrace(printWriter);
-		Throwable cause = ex.getCause();
-		while (cause != null) {
-			cause.printStackTrace(printWriter);
-			cause = cause.getCause();
-		}
-		printWriter.close();
-		String result = writer.toString();
-		contentValues.put("Log",result);
-		DBHelper dbHelper = new DBHelper(App.getInstance());
-		dbHelper.insert(contentValues);
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        printWriter.close();
+        String result = writer.toString();
+        contentValues.put("Log", result);
+        DBHelper dbHelper = new DBHelper(App.getInstance());
+        dbHelper.insert(contentValues);
 
 /*
 		StringBuffer sb = new StringBuffer();
@@ -193,6 +193,6 @@ public class CrashHandler implements UncaughtExceptionHandler {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-	}*/
+		}*/
+    }
 }
